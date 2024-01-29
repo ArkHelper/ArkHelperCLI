fcfee920b174f8d2e4dbd61227c8933ff232ce14
EvATive7 <evative7@gmail.com>
2024-01-30 04:27:24 +0800

    pref: take running maatask out and using callback

--- Libs/process_runner.py
+++ Libs/process_runner.py
@@ -1,3 +1,4 @@
+import queue
 from Libs.MAA.asst.asst import Asst
 from Libs.MAA.asst.utils import Message
 from Libs.MAA.asst.asst import Asst
@@ -29,11 +30,14 @@ def asst_callback(msg, details, arg):
         pass
 
 
-def add_personal_tasks(asst: Asst, config):
-    logging.debug(
-        f'append task with config {config}')
-    for maa_task in config['task']:
-        asst.append_task(maa_task['task_name'], maa_task['task_config'])
+def add_maatasks(asst: Asst, task):
+    for maatask in task['task']:
+        add_maatask(maatask)
+
+
+def add_maatask(asst: Asst, maatask):
+    logging.debug(f'append task {maatask} to {asst}')
+    asst.append_task(maatask['task_name'], maatask['task_config'])
 
 
 class Device:
@@ -41,20 +45,20 @@ class Device:
         self._adb_path = var.global_config['adb_path']
         self._start_path = dev_config['start_path']
         self.emulator_addr = dev_config['emulator_address']
-        self.running_task = None
+        # self.running_task = None
         self.alias = dev_config['alias']
         self._asst = None
         self._connected = False
         self._str = f'device & asst instance {self.alias}({self.emulator_addr})'
         self._current_server = None
-        self._running = False
+        self._current_maatask_status: tuple[Message, dict, object] = (None, None, None)
 
     def __str__(self) -> str:
         return self._str
 
-    def process_callback(self, msg:Message, details:dict, arg):
-        if msg == Message.TaskChainStart:
-            pass
+    def process_callback(self, msg: Message, details: dict, arg):
+        if msg in [Message.TaskChainExtraInfo, Message.TaskChainCompleted, Message.TaskChainError, Message.TaskChainStopped, Message.TaskChainStart]:
+            self._current_maatask_status = (msg, details, arg)
         logging.debug(f'{self} got callback: {msg},{arg},{details}')
 
     def exec_adb(self, cmd: str):
@@ -95,22 +99,49 @@ class Device:
                 self.exec_adb(f'shell am force-stop {arknights_package_name[self._current_server]}')
             self.exec_adb(f'shell am start -n {package_name}/com.u8.sdk.U8UnityContext')
             time.sleep(15)
-        add_personal_tasks(self._asst, task)
-        self._current_server = task_server
-        self._asst.start()
+            self._current_server = task_server
 
-        max_wait_time = 30*60  # second
-        already_waited_time = 0
-        while self._asst.running():
-            interval = 5
-            time.sleep(interval)
-            already_waited_time += interval
-
-            if already_waited_time > max_wait_time:
-                self._asst.stop()
-                break
+        remain_time = 30*60  # sec
+        for maatask in task['task']:
+            run_result = self.run_maatask(maatask, remain_time)
+            remain_time -= run_result['time_cost']
 
         self.exec_adb(f'shell screencap -p /sdcard/DCIM/AkhCLI_{int(time.time())}.png')
+
+    #TODO:need pref
+    def run_maatask(self, maatask, max_time) -> dict:
+        logging.info(f'{self} start run maatask {maatask}')
+        type = maatask['task_name']
+        config = maatask['task_config']
+        this_time_cost = 0
+
+        i = 0
+        max_retry_time = 4
+        for i in range(max_retry_time+1):
+            add_maatask(self._asst, maatask)
+            self._asst.start()
+            while self._asst.running():
+                interval = 5
+                time.sleep(interval)
+                this_time_cost += interval
+                if max_time <= this_time_cost:
+                    self._asst.stop()
+                    break
+            if self._current_maatask_status[0] == Message.TaskChainError:
+                continue
+            else:
+                break
+
+        succeed = self._current_maatask_status[0] == Message.TaskChainCompleted and max_time > this_time_cost
+
+        return {
+            "exec_result": {
+                "succeed": succeed,
+                "reason": self._current_maatask_status[0].name if max_time > this_time_cost else 'timeout',
+                "tried_times": i+1
+            },
+            "time_cost": this_time_cost
+        }
 
 
 def start_process(shared_status, static_process_detail):
