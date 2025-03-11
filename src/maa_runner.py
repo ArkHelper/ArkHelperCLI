ae0f20efaf0bf33d5bc7121bb5b236409eb2a211
EvATive7 <evative7@gmail.com>
2025-03-11 19:10:32 +0800

    style(maa_runner): format code with black and update string quoting
    - Format code using Black formatter for consistent styling
    - Update string quotes to use double quotes consistently
    - Improve readability of complex expressions with line breaks
    - Refactor some code sections for better clarity

--- src/maa_runner.py
+++ src/maa_runner.py
@@ -16,18 +16,18 @@ from indent_concluder import Item as ConcluderItem
 
 
 def do_conclusion():
-    file_name = r'%y-%m-%d-%H-%M-%S.json'
+    file_name = r"%y-%m-%d-%H-%M-%S.json"
     file_name = var.start_time.strftime(file_name)
 
-    file = var.cli_env / 'conclusion' / file_name
+    file = var.cli_env / "conclusion" / file_name
 
     def _get_conclusion():
         return {
-            'msg': 'AkhCLI任务全部完成',
-            'startTime': int(var.start_time.timestamp()*1000),
-            'endTime': int(time.time()*1000),
-            'code': 0,
-            'extra': {}
+            "msg": "AkhCLI任务全部完成",
+            "startTime": int(var.start_time.timestamp() * 1000),
+            "endTime": int(time.time() * 1000),
+            "code": 0,
+            "extra": {},
         }
 
     file.parent.mkdir(exist_ok=True)
@@ -37,14 +37,20 @@ def do_conclusion():
 
 
 def get_report(result):
-    final_conclusion = ConcluderItem('ArkHelperCLI Running')
+    final_conclusion = ConcluderItem("ArkHelperCLI Running")
     for task_id, task_result in result.items():
         if task_result:
-            item = ConcluderItem(task_id, task_result['exec_result']['succeed'], '')
-            for maatask in task_result['exec_result']['maatasks']:
-                item.append(ConcluderItem(maatask['type'], maatask['exec_result']['succeed'], ', '.join(maatask['exec_result']['reason'])))
+            item = ConcluderItem(task_id, task_result["exec_result"]["succeed"], "")
+            for maatask in task_result["exec_result"]["maatasks"]:
+                item.append(
+                    ConcluderItem(
+                        maatask["type"],
+                        maatask["exec_result"]["succeed"],
+                        ", ".join(maatask["exec_result"]["reason"]),
+                    )
+                )
         else:
-            item = ConcluderItem(task_id, False, 'Task failed to run')
+            item = ConcluderItem(task_id, False, "Task failed to run")
         final_conclusion.append(item)
 
     return final_conclusion
@@ -52,8 +58,8 @@ def get_report(result):
 
 def run():
     # update_nav()
-    if var.global_config.get('restart_adb', False):
-        ADB().exec_adb_cmd(['kill-server', 'start-server'])
+    if var.global_config.get("restart_adb", False):
+        ADB().exec_adb_cmd(["kill-server", "start-server"])
     # kill_all_emulators()
 
     @dataclass
@@ -64,11 +70,16 @@ def run():
         process_shared_status: dict | None
         finished: bool
 
-    [var.tasks.append(get_full_task(personal_config)) for personal_config in var.personal_configs]
-    devices = [Device(dev_config) for dev_config in var.global_config['devices']]
-    statuses: list[DeviceStatus] = [DeviceStatus(_device, None, None, None, False) for _device in devices]
-    running_result = {task.get('hash'): None for task in var.tasks}
-    device_count_limit = var.global_config.get('devices_running_limit', 10)
+    [
+        var.tasks.append(get_full_task(personal_config))
+        for personal_config in var.personal_configs
+    ]
+    devices = [Device(dev_config) for dev_config in var.global_config["devices"]]
+    statuses: list[DeviceStatus] = [
+        DeviceStatus(_device, None, None, None, False) for _device in devices
+    ]
+    running_result = {task.get("hash"): None for task in var.tasks}
+    device_count_limit = var.global_config.get("devices_running_limit", 10)
 
     ...
 
@@ -77,14 +88,24 @@ def run():
             if status.process != None:
                 if not status.process.is_alive():
                     task_hash = status.process_static_params["task"]["hash"]
-                    status.device.logger.debug(f'TaskProcess {task_hash} ended, ready to clear')
-                    running_result[task_hash] = status.process_shared_status.get('result', None)
+                    status.device.logger.debug(
+                        f"TaskProcess {task_hash} ended, ready to clear"
+                    )
+                    running_result[task_hash] = status.process_shared_status.get(
+                        "result", None
+                    )
                     status.process = None
                     status.process_static_params = None
                     status.process_shared_status = None
 
         def running_devices_count():
-            return len([_status for _status in statuses if _status.process != None and not _status.finished])
+            return len(
+                [
+                    _status
+                    for _status in statuses
+                    if _status.process != None and not _status.finished
+                ]
+            )
 
         for status in statuses:
             logger = status.device.logger
@@ -96,35 +117,52 @@ def run():
                 _running_devices_count = running_devices_count()
 
                 if _running_devices_count < device_count_limit:
-                    logger.debug(f'{_running_devices_count} devices is running. OK for this')
-                    logger.debug(f'Process is None, ready to distribute task')
+                    logger.debug(
+                        f"{_running_devices_count} devices is running. OK for this"
+                    )
+                    logger.debug(f"Process is None, ready to distribute task")
 
                     def no_task():
-                        logger.debug(f'No task to distribute. Ended')
+                        logger.debug(f"No task to distribute. Ended")
                         if status.device.kill_after_end:
                             status.device.kill()
                         status.finished = True
+
                     if var.tasks:
                         distribute_task = (
-                            [task for task in var.tasks if task.get('device') == status.device.alias] or
-                            [task for task in var.tasks if task.get('device') is None] or
-                            [None]
+                            [
+                                task
+                                for task in var.tasks
+                                if task.get("device") == status.device.alias
+                            ]
+                            or [
+                                task for task in var.tasks if task.get("device") is None
+                            ]
+                            or [None]
                         )[0]
 
                         if distribute_task:
                             var.tasks.remove(distribute_task)
                             process_static_params = {
-                                'task': distribute_task,
-                                'device': status.device
+                                "task": distribute_task,
+                                "device": status.device,
                             }
                             process_shared_status = multiprocessing.Manager().dict()
-                            process = multiprocessing.Process(target=start_task_process, args=(process_static_params, process_shared_status, ))
+                            process = multiprocessing.Process(
+                                target=start_task_process,
+                                args=(
+                                    process_static_params,
+                                    process_shared_status,
+                                ),
+                            )
 
                             status.process = process
                             status.process_static_params = process_static_params
                             status.process_shared_status = process_shared_status
 
-                            logger.debug(f'Ready to start a task process(task={distribute_task["hash"]})')
+                            logger.debug(
+                                f'Ready to start a task process(task={distribute_task["hash"]})'
+                            )
                             process.start()
                         else:
                             no_task()
@@ -132,39 +170,38 @@ def run():
                         no_task()
 
         if all([_status.finished for _status in statuses]):
-            logging.debug(f'All devices ended. Ready to exit')
+            logging.debug(f"All devices ended. Ready to exit")
             break
         else:
             time.sleep(2)
 
     report = get_report(running_result)
     succeed = report.succeed
-    report = '\n'.join([_r.failed_markdown() for _r in report.children])
-    report = \
-        f"""{report}
+    report = "\n".join([_r.failed_markdown() for _r in report.children])
+    report = f"""{report}
 
 {var.start_time.strftime('%Y-%m-%d %H:%M:%S')} - {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"""
-    easywebhooker.configure(var.global_config.get('webhook', []))
+    easywebhooker.configure(var.global_config.get("webhook", []))
     if not succeed:
-        easywebhooker.webhook('run-failed', report=report)
+        easywebhooker.webhook("run-failed", report=report)
     else:
-        easywebhooker.webhook('run-succeed', report=report)
-    easywebhooker.webhook('run-finished', report=report)
+        easywebhooker.webhook("run-succeed", report=report)
+    easywebhooker.webhook("run-finished", report=report)
 
 
 def get_full_task(config: dict):
     final_maatasks: list = []
-    overrides = config.get('override', {})
-    server = config.get('client_type', 'Official')
-    account_name = config.get('account_name', '')
-    template_name = config.get('template', 'default')
-    hash = f'{server}{account_name}'
-    device = var.global_config.get('task-device', {}).get(hash, None)
+    overrides = config.get("override", {})
+    server = config.get("client_type", "Official")
+    account_name = config.get("account_name", "")
+    template_name = config.get("template", "default")
+    hash = f"{server}{account_name}"
+    device = var.global_config.get("task-device", {}).get(hash, None)
 
     template = var.config_templates[template_name]
     for maatask in template:
-        final_task_config: dict = copy.deepcopy(maatask['task_config'])
-        final_task_name = copy.deepcopy(maatask['task_name'])
+        final_task_config: dict = copy.deepcopy(maatask["task_config"])
+        final_task_name = copy.deepcopy(maatask["task_name"])
 
         preference_task_config = overrides.get(final_task_name, {})
 
@@ -178,24 +215,30 @@ def get_full_task(config: dict):
 
                 def aval_expression():
                     def time_between(time_start, time_end):
-                        current_time = datetime.now().strftime('%H:%M')
-                        start_time_obj = datetime.strptime(time_start, '%H:%M')
-                        end_time_obj = datetime.strptime(time_end, '%H:%M')
-                        current_time_obj = datetime.strptime(current_time, '%H:%M')
+                        current_time = datetime.now().strftime("%H:%M")
+                        start_time_obj = datetime.strptime(time_start, "%H:%M")
+                        end_time_obj = datetime.strptime(time_end, "%H:%M")
+                        current_time_obj = datetime.strptime(current_time, "%H:%M")
                         return start_time_obj <= current_time_obj <= end_time_obj
 
                     def date_between(date_start, date_end):
-                        current_date = datetime.now().strftime('%Y-%m-%d')
-                        start_date_obj = datetime.strptime(date_start, '%Y-%m-%d')
-                        end_date_obj = datetime.strptime(date_end, '%Y-%m-%d')
-                        current_date_obj = datetime.strptime(current_date, '%Y-%m-%d')
+                        current_date = datetime.now().strftime("%Y-%m-%d")
+                        start_date_obj = datetime.strptime(date_start, "%Y-%m-%d")
+                        end_date_obj = datetime.strptime(date_end, "%Y-%m-%d")
+                        current_date_obj = datetime.strptime(current_date, "%Y-%m-%d")
                         return start_date_obj <= current_date_obj <= end_date_obj
 
                     def datetime_between(datetime_start, datetime_end):
                         current_datetime = datetime.now()
-                        start_datetime_obj = datetime.strptime(datetime_start, '%Y-%m-%d %H:%M:%S')
-                        end_datetime_obj = datetime.strptime(datetime_end, '%Y-%m-%d %H:%M:%S')
-                        return start_datetime_obj <= current_datetime <= end_datetime_obj
+                        start_datetime_obj = datetime.strptime(
+                            datetime_start, "%Y-%m-%d %H:%M:%S"
+                        )
+                        end_datetime_obj = datetime.strptime(
+                            datetime_end, "%Y-%m-%d %H:%M:%S"
+                        )
+                        return (
+                            start_datetime_obj <= current_datetime <= end_datetime_obj
+                        )
 
                     AM = in_game_time(datetime.now(), server).hour < 12
                     weekday = datetime.now().weekday()
@@ -207,8 +250,8 @@ def get_full_task(config: dict):
                     try:
                         for case in config:
                             case_config = config[case]
-                            if case.replace(' ', '') in ['', 'default']:
-                                case = 'True'
+                            if case.replace(" ", "") in ["", "default"]:
+                                case = "True"
                             case_eval = eval(case, {}, aval_expression())
                             if type(case_eval) != bool:
                                 raise Exception()
@@ -220,36 +263,36 @@ def get_full_task(config: dict):
                         return config
                 else:
                     return config
+
             final_task_config = {key: get_config(key) for key in final_task_config}
 
         def append():
-            if final_task_config.get('enable', True):
-                final_maatasks.append({
-                    'task_name': final_task_name,
-                    'task_config': final_task_config
-                })
+            if final_task_config.get("enable", True):
+                final_maatasks.append(
+                    {"task_name": final_task_name, "task_config": final_task_config}
+                )
 
         update_and_match_case()
-        if final_task_name == 'StartUp':
-            final_task_config['client_type'] = server
-            final_task_config['account_name'] = account_name
-        elif final_task_name == 'Fight':
-            stage = final_task_config.get('stage')
+        if final_task_name == "StartUp":
+            final_task_config["client_type"] = server
+            final_task_config["account_name"] = account_name
+        elif final_task_name == "Fight":
+            stage = final_task_config.get("stage")
             if stage and type(stage) == dict:
-                final_task_config['stage'] = choice_stage(server, stage)
+                final_task_config["stage"] = choice_stage(server, stage)
         else:
             pass
         append()
 
-    if (index := len([t for t in var.tasks if t['hash'] == hash])) != 0:
-        hash += f'_{index}'
+    if (index := len([t for t in var.tasks if t["hash"] == hash])) != 0:
+        hash += f"_{index}"
 
     task = {
-        'hash': hash,
-        'task': final_maatasks,
-        'device': device,
-        'server': server,
-        'account_name': account_name
+        "hash": hash,
+        "task": final_maatasks,
+        "device": device,
+        "server": server,
+        "account_name": account_name,
     }
-    logging.debug(f'Initialization ended for task {hash}: {task}')
+    logging.debug(f"Initialization ended for task {hash}: {task}")
     return task
